190f857ebe5d25fcb1cdda1c5a2a93357730c69d
Jenny <je.sj@hotmail.com>
2019-02-04 16:38:10 +0100

    Avoid copying edge pixels when batch size > 1

--- load_brats_data.py
+++ load_brats_data.py
@@ -366,6 +366,8 @@ def generate_train_batches(root_path, train_list, net_input_shape, net, batchSiz
                 if not np.any(train_mask[:, :, j:j + numSlices * (subSampAmt+1):subSampAmt+1]):
                     continue
                 if img_batch.ndim == 4:
+                    img_batch[count] = 0
+                    mask_batch[count] = 0
                     img_batch[count, 8:-8, 8:-8, :] = train_img[:, :, j:j + numSlices * (subSampAmt+1):subSampAmt+1]
                     mask_batch[count, 8:-8, 8:-8, :] = train_mask[:, :, j:j + numSlices * (subSampAmt+1):subSampAmt+1]
                 elif img_batch.ndim == 5:
